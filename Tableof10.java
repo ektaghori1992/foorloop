f97d9e1f5db23597f7a406fd7ee74301524bf3d5
ekta <aektapatel0102@gmail.com>
2022-08-02 17:47:26 +0100

    my first commit

---
+++ Tableof10.java
@@ -0,0 +1,10 @@
+package ForLoop;
+
+public class Tableof10 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("10 * " + i + " = " + 10*i);
+        }
+    }
+}
---
+++ tenevenodd.java
@@ -0,0 +1,18 @@
+package ForLoop;
+
+public class tenevenodd {
+    public static void main(String[] args) {
+        for(int num=1;num<=20;num++)
+        {
+            if(num%2==0)
+            {
+                System.out.println(num);
+            }
+        }
+        for(int num=1;num<=20;num++)
+         if(num%2==1)
+         {
+             System.out.println(num);
+         }
+    }
+}
